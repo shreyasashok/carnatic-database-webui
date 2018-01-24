e747303bf02ba3c31c6118765299812bd196c36e
Shreyas Ashok <shreyas.ashok1@gmail.com>
2018-01-23 22:14:44 -0500

    Added CORS proxy to solve https issues

--- controller.js
+++ controller.js
@@ -1,6 +1,6 @@
 angular.module('myApp', []).controller('appController', function($scope, $log) {
 	var jquery = $;
-	$.getJSON("http://gsx2json.com/api?id=1LUE00y5QwLVJAgEm2azkiyp75Bc38NVBwkPbN8kEN0U&columns=false", function(data) {
+	$.getJSON("https://cors.io/?http://gsx2json.com/api?id=1LUE00y5QwLVJAgEm2azkiyp75Bc38NVBwkPbN8kEN0U&columns=false", function(data) {
 		$scope.data = data.rows;
 		$log.log(data);
 		$log.log(data.rows[0].violin);
@@ -12,8 +12,5 @@ angular.module('myApp', []).controller('appController', function($scope, $log) {
 	});
 	
 	$scope.tab = 0;
-	$scope.setTab = function(newTab) {
-		$scope.tab = newTab;
-	}
 	
 });
