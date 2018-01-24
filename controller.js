2843b73b7d5d15f86c944aa88a1f12fbc6b263a5
Shreyas Ashok <shreyas.ashok1@gmail.com>
2018-01-23 23:58:30 -0500

    Created directives and changed sheet access to direct method

--- controller.js
+++ controller.js
@@ -1,16 +1,36 @@
-angular.module('myApp', []).controller('appController', function($scope, $log) {
+angular.module('myApp', [])
+.controller('appController', function($scope, $log) {
 	var jquery = $;
-	$.getJSON("https://cors.io/?http://gsx2json.com/api?id=1LUE00y5QwLVJAgEm2azkiyp75Bc38NVBwkPbN8kEN0U&columns=false", function(data) {
-		$scope.data = data.rows;
-		$log.log(data);
-		$log.log(data.rows[0].violin);
-		$scope.$apply(function() {
-			$scope.testmodel = data.rows[0].violin;
-		});
-		$log.log("log 2"+$scope.data);
-		$log.log($scope.name);
+	$.getJSON("https://spreadsheets.google.com/feeds/list/1LUE00y5QwLVJAgEm2azkiyp75Bc38NVBwkPbN8kEN0U/od6/public/values?alt=json", function(data) {
+		var rawData = data.feed.entry;
+		$log.log(rawData);
+
+		$log.log(rawData[0]);
+		$scope.data = [];
+		for (var rawIndex in rawData) {
+			var rawRow = rawData[rawIndex];
+			var processedRow = {albumid:rawRow.gsx$albumid.$t, mainartist:rawRow.gsx$mainartist.$t, violinist:rawRow.gsx$violin.$t, mridangist:rawRow.gsx$mridangist.$t};
+			$scope.$apply( function() {
+				$scope.data.push(processedRow);
+			});
+		}
 	});
 	
 	$scope.tab = 0;
 	
+})
+.directive("trackList", function() {
+	return {
+		templateUrl: "trackListTemplate.html"
+	};
+})
+.directive("albums", function() {
+	return {
+		templateUrl: "albumsTemplate.html"
+	}
+})
+.directive("playlist", function() {
+	return {
+		templateUrl: "playlistTemplate.html"
+	}
 });
