5b18c6f913a92cfd7a263607b8bba11e5bf73cac
Shreyas Ashok <shreyas.ashok1@gmail.com>
2018-02-01 17:46:23 -0500

    Significant progress on track view

--- controller.js
+++ controller.js
@@ -1,7 +1,9 @@
 angular.module('myApp', [])
 .controller('appController', function($scope, $log) {
 	var jquery = $;
-	$.getJSON("https://spreadsheets.google.com/feeds/list/1LUE00y5QwLVJAgEm2azkiyp75Bc38NVBwkPbN8kEN0U/1/public/values?alt=json", function(data) {
+	var week = 604800000;
+	var spreadsheet_key = "1MHBp2GX_h5B9mC6GPcIt7XH2-CPz4W8FK0f2nBi9nZ4"
+	$.getJSON("https://spreadsheets.google.com/feeds/list/"+spreadsheet_key+"/2/public/values?alt=json", function(data) {
 		var rawData = data.feed.entry;
 		$log.log(rawData);
 
@@ -9,13 +11,15 @@ angular.module('myApp', [])
 		$scope.albumData = [];
 		for (var rawIndex in rawData) {
 			var rawRow = rawData[rawIndex];
-			var processedRow = {albumid:rawRow.gsx$albumid.$t, mainartist:rawRow.gsx$mainartist.$t, violinist:rawRow.gsx$violin.$t, mridangist:rawRow.gsx$mridangist.$t};
+			var processedRow = {albumid:rawRow.gsx$albumid.$t, mainartist:rawRow.gsx$mainartist.$t, violin:rawRow.gsx$violin.$t, mridangam:rawRow.gsx$mridangam.$t, upapakkavadhyam:rawRow.gsx$upapakkavadhyam.$t,
+						concertsabha:rawRow.gsx$concertsabha.$t, sabhalocation:rawRow.gsx$sabhalocation.$t, date:rawRow.gsx$date.$t, audioquality:rawRow.gsx$audioquality.$t, uploaddate:rawRow.gsx$uploaddate.$t};
+			$log.log(rawRow.gsx$upapakkavadhyam.$t)
 			$scope.$apply( function() {
 				$scope.albumData.push(processedRow);
 			});
 		}
 	});
-	$.getJSON("https://spreadsheets.google.com/feeds/list/1LUE00y5QwLVJAgEm2azkiyp75Bc38NVBwkPbN8kEN0U/2/public/values?alt=json", function(data) {
+	$.getJSON("https://spreadsheets.google.com/feeds/list/"+spreadsheet_key+"/1/public/values?alt=json", function(data) {
 		var rawData = data.feed.entry;
 		$log.log(rawData);
 
@@ -23,7 +27,19 @@ angular.module('myApp', [])
 		$scope.krithiData = [];
 		for (var rawIndex in rawData) {
 			var rawRow = rawData[rawIndex];
-			var processedRow = {songtitle:rawRow.gsx$songtitle.$t, albumid:rawRow.gsx$albumid.$t, tracknumber:rawRow.gsx$tracknumber.$t};
+			var processedRow = {songtitle:rawRow.gsx$songtitle.$t, albumid:rawRow.gsx$albumid.$t, tracknumber:rawRow.gsx$tracknumber.$t, type:rawRow.gsx$type.$t, ragam:rawRow.gsx$ragam.$t, talam:rawRow.gsx$talam.$t, 
+						composer:rawRow.gsx$composer.$t, alapana:rawRow.gsx$a.$t, niraval:rawRow.gsx$n.$t, swaram:rawRow.gsx$s.$t, comments:rawRow.gsx$comments.$t, youtubevideoid:rawRow.gsx$youtubevideoid.$t, 
+						mainartist:rawRow.gsx$mainartist.$t, violin:rawRow.gsx$violin.$t, mridangam:rawRow.gsx$mridangam.$t, upapakkavadhyam:rawRow.gsx$upapakkavadhyam.$t, concertsabha:rawRow.gsx$concertsabha.$t,
+						sabhalocation:rawRow.gsx$sabhalocation.$t, date:rawRow.gsx$date.$t, audioquality:rawRow.gsx$audioquality.$t, uploaddate:rawRow.gsx$uploaddate.$t, newtrack: false};
+			var uploadDate = new Date(processedRow.uploaddate);
+			var today = new Date();
+			var delta = today-uploadDate;
+			if (delta < week) {
+				processedRow.newtrack = true;
+			}
+			$log.log(delta);
+			$log.log(uploadDate.toUTCString());
+			$log.log(processedRow.newtrack);
 			$scope.$apply( function() {
 				$scope.krithiData.push(processedRow);
 			});
@@ -38,10 +54,11 @@ angular.module('myApp', [])
 	$scope.krithiSortReverse = false;
 	$scope.albumSortType = "albumid";
 	$scope.albumSortReverse = false;
+	$scope.krithiColumnEnable = {albumid: true, tracknumber: true, songtitle: true, type: false, ragam: true, talam: true, composer: true, comments: false, mainartist:true, violin:true, mridangam: true, upapakkavadhyam: false, 
+					concertsabha: false, sabhalocation: false, date: false};
+        $log.log($scope.krithiColumnEnable.sabhalocation);
+	$scope.albumColumnEnable = {};
 
-	$scope.logFunction = function($print) {
-		$log.log($print);
-	};
 
 	$scope.clearKrithiSearch = function() {
 		$scope.krithiSearch.songtitle = "";
@@ -62,6 +79,28 @@ angular.module('myApp', [])
 		$scope.krithiSortReverse = false;
 		$scope.tab = 0;
 	};
+	$scope.krithiInfoButtonClick = function($row) {
+		$scope.krithiModalContent = $row.songtitle+", "+$row.ragam;
+		$('#krithiInfoModal').modal()
+	};
+	$scope.showHideKrithiColumn = function($column, $showHide, $search) {
+		if ($showHide == false) {
+			$scope.krithiSearch[$column] = "";
+			if ($scope.krithiSortType === $column) {
+				$scope.krithiSortType = "songtitle";
+			}
+		}
+		
+	};
+	$scope.changeKrithiSort = function($newSort) {
+		if ($newSort === $scope.krithiSortType) {
+			$scope.krithiSortReverse = !$scope.krithiSortReverse;
+		}
+		else {
+			$scope.krithiSortType = $newSort;
+			$scope.krithiSortReverse = false;
+		}
+	};
 
 })
 .directive("trackList", function() {
@@ -72,10 +111,10 @@ angular.module('myApp', [])
 .directive("albums", function() {
 	return {
 		templateUrl: "albumsTemplate.html"
-	}
+	};
 })
 .directive("playlist", function() {
 	return {
 		templateUrl: "playlistTemplate.html"
-	}
+	};
 });
